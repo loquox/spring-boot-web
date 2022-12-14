f59a81438bc85b8ef8c26aaff96a7864b68d8e51
loquox <crloquox@gmail.com>
2022-12-14 12:08:22 +0100

    se han hechio muchos cambios

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,12 +20,22 @@ import com.bolsadeideas.springboot.web.app.models.Usuario;
 @RequestMapping("/app")
 public class IndexController {
 	
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textolistar;
+	
 	
 	@RequestMapping(value = {"/","/index","/home",""}, method = RequestMethod.GET)
 	
 	public String index(Model model) {
-		model.addAttribute("titulo","Hola Spring Framework con Model");
-				
+		//model.addAttribute("titulo","Hola Spring Framework con Model");
+		//es lo mismo que arriba pero haciendo uso de @Value para incluir el valor
+		 model.addAttribute("titulo",textoIndex);
+		
+		
 		return "index";
 	}
 	
@@ -35,14 +46,18 @@ public class IndexController {
 		usuario.setApellido("Ramos");
 		usuario.setEmail("carlosramos@hotmail.com");
 		model.addAttribute("usuario",usuario);
-		model.addAttribute("titulo","Perfil del usuario: ".concat(usuario.getNombre()));
-		
+		//model.addAttribute("titulo","Perfil del usuario: ".concat(usuario.getNombre()));
+		//es lo mismo que arriba pero haciendo uso de @Value para incluir el valor
+		model.addAttribute("titulo",textoPerfil.concat(usuario.getNombre()));
 		return "perfil";
 	}
 	
 	@RequestMapping("/listar")
 	public String listar(Model model) {
-		model.addAttribute("titulo", "Listado de usuarios ");
+		//model.addAttribute("titulo", "Listado de usuarios ");
+		//es lo mismo que arriba pero haciendo uso de @Value para incluir el valor
+		model.addAttribute("titulo",textolistar);
+		
 		//Esta es una forma de meter usuarios a lista la otra está abajo
 		/*List<Usuario> usuarios = new ArrayList<>();
 		usuarios.add(new Usuario("Luis","Garcia","luisgarcia@gmail"));
@@ -54,6 +69,7 @@ public class IndexController {
 				new Usuario("John","Sistiaga","jon@gmail.com"),
 				new Usuario("JIhn","Ramos","jIn@gmail.com"),
 				new Usuario("Carlos","Ramos","crlo@mslsl.com"));
+		//quitamos el model usuarios para usarlo abajo con @ModelAttribute 
 		//model.addAttribute("usuarios", usuarios);
 		return "listar";
 	}
